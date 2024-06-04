f1cd25278aa6a51ee302d7cb9fb9ee670ad14af8
0xVendetta <thevendetta0x@gmail.com>
2024-06-04 06:55:07 +0100

    near.sh

---
+++ near.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://near.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+
+OUTPUT_DIR="logs"
+
+near_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"block","id":"HappyCuanAirdrop","params":{"finality":"final"}}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$near_data" >> "$OUTPUT_DIR/near.txt"
+    
+    echo "NEAR data fetched and saved to $OUTPUT_DIR/near.txt."
+else
+    echo "Failed to fetch NEAR data."
+fi
