97710215e75aff6da0af3d408ad6cdec84115ea1
0xVendetta <thevendetta0x@gmail.com>
2024-06-04 06:52:47 +0100

    ethereum.sh

---
+++ ethereum.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://eth1.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+
+OUTPUT_DIR="logs"
+
+eth_data=$(curl -s -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_gasPrice","id":1}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$eth_data" >> "$OUTPUT_DIR/ethereum.txt"
+    
+    echo "Ethereum data fetched and saved to $OUTPUT_DIR/ethereum.txt."
+else
+    echo "Failed to fetch Ethereum data."
+fi
