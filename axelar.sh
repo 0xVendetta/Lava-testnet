497c5c00448aec93801fe79823f735f3efad0b00
0xVendetta <thevendetta0x@gmail.com>
2024-06-04 06:51:11 +0100

    axelar.sh

---
+++ axelar.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RPC_URL="https://tm.axelar.lava.build/lava-referer-00c6ad4a-5ae1-4fcd-b0ab-703adcd0635e/"
+
+OUTPUT_DIR="logs"
+
+axelar_data=$(curl -s -H "Content-Type: application/json" -X POST --data '{"jsonrpc":"2.0","id":"1","method":"status"}' "$RPC_URL")
+
+if [ $? -eq 0 ]; then
+    if [ ! -d "$OUTPUT_DIR" ]; then
+        mkdir "$OUTPUT_DIR"
+    fi
+    
+    echo "$axelar_data" >> "$OUTPUT_DIR/axelar.txt"
+    
+    echo "Axelar data fetched and saved to $OUTPUT_DIR/axelar.txt."
+else
+    echo "Failed to fetch Axelar data."
+fi
